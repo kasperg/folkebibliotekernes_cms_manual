6cc0343bbf915f0e4cee5739f273520aaa7c1b12
kasperg <kasperg@users.noreply.github.com>
2023-07-04 07:44:33 +0000

    Deploy preview for PR 1 🛫

--- pr-preview/pr-1/assets/js/lunr/lunr-store.js
+++ pr-preview/pr-1/assets/js/lunr/lunr-store.js
@@ -2,42 +2,42 @@ var store = [{
         "title": "Administrator",
         "excerpt":"Administrator  ","categories": [],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/audience/administrator/",
+        "url": "/pr-previews/pr-1/audience/administrator/",
         "teaser": null
       },{
         "title": "Redaktør",
         "excerpt":"Redaktør  ","categories": [],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/audience/redaktoer/",
+        "url": "/pr-previews/pr-1/audience/redaktoer/",
         "teaser": null
       },{
         "title": "Introduktion",
         "excerpt":"Forside, læsevejledning  ","categories": [],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/category/introduktion/",
+        "url": "/pr-previews/pr-1/category/introduktion/",
         "teaser": null
       },{
         "title": "CMS Manual",
         "excerpt":"I løbet af efteråret udbygges manual til Folkebibliotekernes CMS på disse sider. Vi har valgt en åben proces, hvor bibliotekerne kan se manual-siderne, efterhånden som de bliver skrevet.  ","categories": ["Introduktion"],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/manual/",
+        "url": "/pr-previews/pr-1/manual/",
         "teaser": null
       },{
         "title": "Betaling",
         "excerpt":"Betaling  ","categories": [],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/topic/betaling/",
+        "url": "/pr-previews/pr-1/topic/betaling/",
         "teaser": null
       },{
         "title": "Formidling",
         "excerpt":"Formidling  ","categories": [],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/topic/formidling/",
+        "url": "/pr-previews/pr-1/topic/formidling/",
         "teaser": null
       },{
         "title": "Grundkonfiguration",
         "excerpt":"Grundkonfiguration  ","categories": [],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/topic/grundkonfiguration/",
+        "url": "/pr-previews/pr-1/topic/grundkonfiguration/",
         "teaser": null
       }]
