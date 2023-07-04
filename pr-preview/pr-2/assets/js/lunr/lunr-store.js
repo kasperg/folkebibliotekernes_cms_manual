521bf35d761ffc69865d2b625d1e2034a5f13a3c
kasperg <kasperg@users.noreply.github.com>
2023-07-04 09:55:37 +0000

    Deploy preview for PR 2 🛫

---
+++ pr-preview/pr-2/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Administrator",
+        "excerpt":"Administrator  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/pr-preview/pr-2/audience/administrator/",
+        "teaser": null
+      },{
+        "title": "Redaktør",
+        "excerpt":"Redaktør  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/pr-preview/pr-2/audience/redaktoer/",
+        "teaser": null
+      },{
+        "title": "Introduktion",
+        "excerpt":"Forside, læsevejledning  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/pr-preview/pr-2/category/introduktion/",
+        "teaser": null
+      },{
+        "title": "CMS Manual",
+        "excerpt":"I løbet af efteråret udbygges manual til Folkebibliotekernes CMS på disse sider. Vi har valgt en åben proces, hvor bibliotekerne kan se manual-siderne, efterhånden som de bliver skrevet.  ","categories": ["Introduktion"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/pr-preview/pr-2/manual/",
+        "teaser": null
+      },{
+        "title": "Betaling",
+        "excerpt":"Betaling  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/pr-preview/pr-2/topic/betaling/",
+        "teaser": null
+      },{
+        "title": "Formidling",
+        "excerpt":"Formidling  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/pr-preview/pr-2/topic/formidling/",
+        "teaser": null
+      },{
+        "title": "Grundkonfiguration",
+        "excerpt":"Grundkonfiguration  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/pr-preview/pr-2/topic/grundkonfiguration/",
+        "teaser": null
+      }]
